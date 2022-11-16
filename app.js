8482fba85d9d9dea04c55bb83c614f310d42b9f5
M.A <macarenaamato00@gmail.com>
2022-11-16 12:49:14 -0300

    Modif

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require ("path");
 const app = express();
 
+
 app.use(express.static("public"));
 
 app.listen (3000, () => console.log ("Servidor corriendo"));
